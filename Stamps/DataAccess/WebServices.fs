46a9940cff878dee471500e595297f6451b69250
Savelenko <savelenko@gmail.com>
2016-07-29 01:44:17 +0200

    Added the data access layer. Not implemented yet.

---
+++ Stamps/DataAccess/WebServices.fs
@@ -0,0 +1,10 @@
+﻿namespace Stamps.DataAccess
+
+open System
+open Stamps.Model
+
+type WorldStampKnowledgeBaseService() =
+
+    interface IStampRarenessService with
+        
+        member svc.VerifyRareness description = raise (NotImplementedException "Service not yet implemented.")
